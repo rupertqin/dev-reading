378ab986c1b567022b834cfd1ac72b125e81a614
Qin Xiang <rupert.qing@gmail.com>
2016-12-01 18:06:34 +0800

    add sufolder that server static dir

--- lib/dev_reading/endpoint.ex
+++ lib/dev_reading/endpoint.ex
@@ -9,7 +9,7 @@ defmodule DevReading.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :dev_reading, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css dist semantic fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
